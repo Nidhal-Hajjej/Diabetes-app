9f4d247cc0f1f718d8f0d9b3e716bdde31adea49
Nidhal-Hajjej <nidhalhj16@gmail.com>
2025-06-15 23:05:28 +0100

    Adding missing files

--- app/Http/Controllers/DiabetesController.php
+++ app/Http/Controllers/DiabetesController.php
@@ -15,10 +15,18 @@ class DiabetesController extends Controller
     public function index(Request $request)
     {
         if ($request->isMethod('post')) {
+<<<<<<< HEAD
             $streamlit_output = shell_exec('streamlit run /predictionSystem/app.py --server.port 8080');
             return view('prediction', ['output' => $streamlit_output]);
         }
 
+=======
+            $form_data = $request->all();
+            $streamlit_url = 'http://localhost:8501/?' . http_build_query($form_data);
+            return view('prediction', ['streamlit_url' => $streamlit_url]);
+        }
+    
+>>>>>>> dev-ahmed
         return view('prediction');
     }
 }
